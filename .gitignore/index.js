c76a21e57c53200519d1622f8c0a18e7b8559ee0
kovaleski1 <37919923+kovaleski1@users.noreply.github.com>
2018-03-31 03:55:08 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,50 +1,14 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client();
-const low = require('lowdb')
-const FileSync = require ('lowdb/adapters/FileSync')
 const client = new Discord.Client();
 
-const adapter = new FileSync('database.json')
-const db = low(adapter);
-
-db.defaults({ histoires: [], xp: []}).write()
-
-var prefix = ("ç")
-
-bot.on('ready', function() {
-    bot.user.setGame("Command: çhelp");
-    console.log("Connected");
+client.on("ready", () => {
+  console.log("I am ready!");
 });
 
-bot.login("NDI3OTY2Njg3Mjk2NjE4NTA2.DZ-jmg.Nj4Uqeh6LzzvWs9uLJtNgrGB7dA");
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
 
-
-bot.on('message', message => {
-
-    var msgauthor = message.author.id;
-
-    if(message.author.bot) return;
-
-    if(!db.get("xp").find({user: msgauthor}).value()){
-        db.get("xp").push({user: msgauthor, xp: 1}).write();
-    }else{
-        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
-        console.log(userxpdb)
-        var userxp = Object.values(userxpdb)
-        console.log(userxp)
-        console.log(`Nombre d'xp: ${userxp[1]}`)
-
-        db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
-
-    if (message.content === prefix + "xp"){
-        var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
-        var xpfinal = Object.values(xp);
-        var xp_embed = new Discord.RichEmbed()
-            .setTitle(`Affiche de ${message.author.username}`)
-            .setColor('#F4D03F')
-            .setDescription("")
-            .addField("Nombre total de messages:", `${xpfinal[1]} messages`)
-            .setFooter("Pensez à nous envoyer vos suggestion dans la partie adéquate")
-        message.channel.send({embed: xp_embed});
-
-      }}})        
+client.login("NDI3OTY2Njg3Mjk2NjE4NTA2.DZ-jmg.Nj4Uqeh6LzzvWs9uLJtNgrGB7dA");
